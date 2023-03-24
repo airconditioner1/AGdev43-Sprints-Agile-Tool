afd2f21ac73b7174a376d97e85e31996d63e811f
cass9918 <71353448+cass9918@users.noreply.github.com>
2023-03-23 20:30:08 -0500

    Database v1

--- sqlconnect.py
+++ sqlconnect.py
@@ -1,24 +1,55 @@
 import mysql.connector              #pip install mysql-connector-python
 from mysql.connector import errorcode
+import json
+from datetime import date, datetime
 
-try:
-  cnx = mysql.connector.connect(user='root', password='mc', host='localhost', port=63306, database='ThisAndThat')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cursor = cnx.cursor()
+# Query strings
+q_all_cards_by_proj = "select Title, DueDate, Hours, Priority, Status from Task where PNum = {:d};"
 
-  query = ("select *from This")
+# JSON serializer for objects not serializable by default json code
+def json_serial(obj):
+  if isinstance(obj, (datetime, date)):
+    return obj.isoformat()
+  raise TypeError ("Type %s not serializable" % type(obj))
 
-  cursor.execute(query)
+class DBAccess:
 
-  for (int1, str1, int2, date, str2) in cursor:
-    print("Int - {}, String - {}, Int - {}, Date - {:%d %b %Y}, String - {}".format(int1, str1, int2, date, str2))
+  def __init__(self):
+    try:
+      self.cnx = mysql.connector.connect(user='root', password='mc', host='localhost', port=3306, database='AGDev43')
+      self.cursor = self.cnx.cursor(dictionary=True)
+    except mysql.connector.Error as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+      elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+      else:
+        print(err)
+    else:
+      self.cursor = self.cnx.cursor(dictionary=True)
+  
+  def execute_query(self, query):
+    dictionary = []
 
-  cursor.close() 
-  cnx.close()
+    self.cursor.execute(query)
+    for row in self.cursor:
+      dictionary.append(row)
+
+    jsonret = json.dumps(dictionary, indent=2, default=json_serial)
+
+    return(jsonret)
+  
+  def all_cards_by_proj(self, pnum):
+    return self.execute_query(q_all_cards_by_proj.format(pnum))
+
+  def __del__(self):
+    self.cursor.close() 
+    self.cnx.close()
+
+def main():
+  db = DBAccess()
+  jsonret = db.all_cards_by_proj(1)
+  print(jsonret)
+
+if __name__ == "__main__":
+  main()
