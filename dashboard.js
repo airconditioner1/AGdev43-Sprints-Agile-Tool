95ee0ee0cda62b8e206b95a99c4ea7e928e3a8e0
airconditioner1 <allenfchen@yahoo.com>
2023-03-25 18:27:36 -0500

    transfer work from old respository

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+let btnCreateATask = document.getElementById("createATask")
+let taskForm = document.getElementById("box-taskForm")
+function createATask() {
+    btnCreateATask.innerText = "Creating Task"
+    taskForm.innerHTML = "<"
+
+}
