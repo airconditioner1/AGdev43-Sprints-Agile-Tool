ab1af4ca1ff217e3854d67d42d00ca2764e157e8
Irving Peng <cpeng53@wisc.edu>
2023-04-17 20:19:51 -0500

    Upload New File

---
+++ story_poker/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+return (
+	<div>
+	<h1>Estimate the hour needed to read Chapter 7 of Agile Developent?</h1>
+	</div>
+);
+};
+
+export default Contact;
