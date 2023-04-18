d96a4d74f7d963c82d7e645c0410ebbac0c28aeb
Irving Peng <cpeng53@wisc.edu>
2023-04-17 20:20:17 -0500

    Upload New File

---
+++ story_poker/pages/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+return (
+	<div>
+	<h1>Estimate the hour needed to create a database with all the users' data</h1>
+	</div>
+);
+};
+
+export default SignUp;
